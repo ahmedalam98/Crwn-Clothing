b463849ee7dd218060f104bef029d8a2c6f443e7
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-04-09 14:15:28 +0200

    feat: :sparkles: Add feature add to cart

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,12 +1,40 @@
 import { createContext, useState } from "react";
 
+// helper function to check if the product exist to increase the quantity, or not to add the product itself
+const checkCartItem = (cartItems, productToAdd) => {
+  // find if cartItems contains productToAdd
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  // if found, increment quantity
+  if (existingCartItem) {
+    return cartItems.map((cartItem) =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // if not, return new array with modified cartItems / new cart item after clicking ( Add To Cart button )
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(checkCartItem(cartItems, productToAdd));
+  };
+
+  const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
