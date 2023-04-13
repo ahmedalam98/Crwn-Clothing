a667219319e1aadf3633fa456c9c61c372120d5e
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-04-13 02:43:18 +0200

    refactor: :fire: Disable user context code after migrating into redux store

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,55 +1,55 @@
-import { createContext, useReducer } from "react";
+// import { createContext, useReducer } from "react";
 
-import { createAction } from "../utils/reducer/reducer.utils";
+// import { createAction } from "../utils/reducer/reducer.utils";
 
-// the actual values we want to access
-export const UserContext = createContext({
-  currentUser: null,
-  setCurrentUser: () => null,
-});
+// // the actual values we want to access
+// export const UserContext = createContext({
+//   currentUser: null,
+//   setCurrentUser: () => null,
+// });
 
-// the object of all user context actions
-export const USER_ACTION_TYPES = {
-  SET_CURRENT_USER: "SET_CURRENT_USER",
-};
+// // the object of all user context actions
+// export const USER_ACTION_TYPES = {
+//   SET_CURRENT_USER: "SET_CURRENT_USER",
+// };
 
-// utilizing useReducer 1) : initial state value
-const INITIAL_STATE = {
-  currentUser: null,
-};
+// // utilizing useReducer 1) : initial state value
+// const INITIAL_STATE = {
+//   currentUser: null,
+// };
 
-const userReducer = (state, action) => {
-  const { type, payload } = action;
-  switch (type) {
-    // action for the function we need to access in user context
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      // return an object of all the state values + the value we want to update
-      return {
-        ...state,
-        // state value inside reducer that will be modified
-        currentUser: payload,
-      };
-    default:
-      throw new Error(`Unhandled type ${type} in useReduer`);
-  }
-};
+// const userReducer = (state, action) => {
+//   const { type, payload } = action;
+//   switch (type) {
+//     // action for the function we need to access in user context
+//     case USER_ACTION_TYPES.SET_CURRENT_USER:
+//       // return an object of all the state values + the value we want to update
+//       return {
+//         ...state,
+//         // state value inside reducer that will be modified
+//         currentUser: payload,
+//       };
+//     default:
+//       throw new Error(`Unhandled type ${type} in useReduer`);
+//   }
+// };
 
-export const UserProvider = ({ children }) => {
-  // utilizing useReducer 2) :
-  // NOTE 1 : state, dispatch ---> always return from useReducer
-  // NOTE 2 : userReducer, INITIAL_STATE ---> cuz you pass to reducers the actual reducer & initial state of context you wanna modify
-  // Note 3 : dispatch is a function that always recieve an *action* and *payload* (if found) as arguments
-  const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
-  const { currentUser } = state;
+// export const UserProvider = ({ children }) => {
+//   // utilizing useReducer 2) :
+//   // NOTE 1 : state, dispatch ---> always return from useReducer
+//   // NOTE 2 : userReducer, INITIAL_STATE ---> cuz you pass to reducers the actual reducer & initial state of context you wanna modify
+//   // Note 3 : dispatch is a function that always recieve an *action* and *payload* (if found) as arguments
+//   const [state, dispatch] = useReducer(userReducer, INITIAL_STATE);
+//   const { currentUser } = state;
 
-  const setCurrentUser = (user) => {
-    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
-  };
+//   const setCurrentUser = (user) => {
+//     dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
+//   };
 
-  const value = { currentUser, setCurrentUser };
+//   const value = { currentUser, setCurrentUser };
 
-  // centralized place of Auth Listener
-  // hook up to stream of some events
+//   // centralized place of Auth Listener
+//   // hook up to stream of some events
 
-  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
-};
+//   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+// };
