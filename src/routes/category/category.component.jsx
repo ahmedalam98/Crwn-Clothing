01b61a633c47a10dbe8144af1397b83250f19d15
Ahmed Alam El-Deen <ahmed.alameldeen.me@gmail.com>
2023-04-18 16:43:12 +0200

    feat: :sparkles: Use isLoading to render spinner

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -2,9 +2,13 @@ import { useState, useEffect, Fragment } from "react";
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-import { selectCategoriesMap } from "../../store/categories/categories.selector";
+import {
+  selectCategoriesMap,
+  selectIsLoading,
+} from "../../store/categories/categories.selector";
 
 import ProductCard from "../../components/product-card/product-card.component";
+import Spinner from "../../components/spinner/spinner.component";
 
 import { CategoryContainer, CategoryTitle } from "./category.styles";
 
@@ -13,6 +17,7 @@ const Category = () => {
   const { category } = useParams();
 
   const categoriesMap = useSelector(selectCategoriesMap);
+  const isLoading = useSelector(selectIsLoading);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
@@ -22,13 +27,17 @@ const Category = () => {
   return (
     <Fragment>
       <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
-      <CategoryContainer>
-        {/* building a safeguard to only render products.map if products != undefined because products.map rely on async fetched code */}
-        {products &&
-          products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-      </CategoryContainer>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <CategoryContainer>
+          {/* building a safeguard to only render products.map if products != undefined because products.map rely on async fetched code */}
+          {products &&
+            products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
+        </CategoryContainer>
+      )}
     </Fragment>
   );
 };
